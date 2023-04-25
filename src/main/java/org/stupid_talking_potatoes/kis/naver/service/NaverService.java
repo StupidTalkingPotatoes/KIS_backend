0b7ec1e9c464036b75c06772a41144aca0defbf6
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-04-25 23:10:32 +0900

    [ADD] Naver 빠른 길찾기 api 요청을 위한 메서드 작성

--- src/main/java/org/stupid_talking_potatoes/kis/naver/service/NaverService.java
+++ src/main/java/org/stupid_talking_potatoes/kis/naver/service/NaverService.java
@@ -1,9 +1,11 @@
 package org.stupid_talking_potatoes.kis.naver.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.stupid_talking_potatoes.kis.path.dto.Path;
 import org.stupid_talking_potatoes.kis.route.domain.ArrivalRoute;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +16,22 @@ import java.util.List;
  * date : 2023-04-23
  */
 @Service
+@Slf4j
 public class NaverService {
     public ArrayList<Path> getTransportationInfo(Double departureLongitude,Double departureLatitude,Double arrivalLongitude,Double arrivalLatitude){
-        return  null;
+        return null;
     }
+    private String buildUrl(Double departureLongitude, Double departureLatitude, Double arrivalLongitude, Double arrivalLatitude) {
+        String baseUrl = "https://pt.map.naver.com/api/pubtrans-search";
+        StringBuilder urlBuilder = new StringBuilder(baseUrl);
+        urlBuilder.append("?");
+        urlBuilder.append("&mode=TIME");
+        urlBuilder.append("&departureTime=").append(LocalDateTime.now());
+        urlBuilder.append("&departure=").append(departureLongitude).append(",").append(departureLatitude);
+        urlBuilder.append("&arrival=").append(arrivalLongitude).append(",").append(arrivalLatitude);
+        return urlBuilder.toString();
+    }
+
     private  ArrayList<Path> filterPath(List<ArrivalRoute> arrivalRouteList){
         return null;
     }
