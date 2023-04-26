52a70cb1ee76f5e0df76d05dbb6a39d16adbfc41
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-04-26 19:57:47 +0900

    hard fix

--- src/main/java/org/stupid_talking_potatoes/kis/naver/service/NaverService.java
+++ src/main/java/org/stupid_talking_potatoes/kis/naver/service/NaverService.java
@@ -1,10 +1,17 @@
 package org.stupid_talking_potatoes.kis.naver.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.stupid_talking_potatoes.kis.path.dto.Path;
+import org.stupid_talking_potatoes.kis.path.dto.Step;
 import org.stupid_talking_potatoes.kis.route.domain.ArrivalRoute;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +26,49 @@ import java.util.List;
 @Slf4j
 public class NaverService {
     public ArrayList<Path> getTransportationInfo(Double departureLongitude,Double departureLatitude,Double arrivalLongitude,Double arrivalLatitude){
+        JSONObject ptMapJSON = getPtMapJSON(departureLongitude, departureLatitude, arrivalLongitude, arrivalLatitude);
+        return jsonToDto(ptMapJSON);
+    }
+    
+    private ArrayList<Path> jsonToDto(JSONObject json) {
+        int jsonLength=json.getJSONObject("res").getJSONArray("paths").length();
+        for (int i = 0; i < jsonLength; i++) {
+            Path path=new Path();
+            path.setDuration((Integer) json.getJSONObject("res").getJSONArray("paths").getJSONObject(0).get("duration"));
+            Step step=new Step();
+            json.getJSONObject("res").getJSONArray("paths").getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONArray("steps").getJSONObject(0).toString();
+            path.setStepList(null);
+        }
         return null;
     }
+    
+    public JSONObject getPtMapJSON(Double departureLongitude, Double departureLatitude, Double arrivalLongitude, Double arrivalLatitude) {
+        String url = buildUrl(departureLongitude, departureLatitude, arrivalLongitude, arrivalLatitude);
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            log.info("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            return new JSONObject(sb.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
     private String buildUrl(Double departureLongitude, Double departureLatitude, Double arrivalLongitude, Double arrivalLatitude) {
         String baseUrl = "https://pt.map.naver.com/api/pubtrans-search";
         StringBuilder urlBuilder = new StringBuilder(baseUrl);
--- src/test/java/org/stupid_talking_potatoes/kis/naver/service/NaverServiceTest.java
+++ src/test/java/org/stupid_talking_potatoes/kis/naver/service/NaverServiceTest.java
@@ -1,10 +1,22 @@
 package org.stupid_talking_potatoes.kis.naver.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.json.BasicJsonTester;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * package :  org.stupid_talking_potatoes.kis.naver.service
@@ -13,19 +25,57 @@ import static org.junit.jupiter.api.Assertions.*;
  * date : 2023-04-25
  */
 @Slf4j
+@ExtendWith(MockitoExtension.class)
 class NaverServiceTest {
     private final NaverService naverService = new NaverService();
+    private final BasicJsonTester json = new BasicJsonTester(getClass());
+    JSONObject res;
+    
+    @BeforeEach
+    @DisplayName("빠른 길찾기 api 연결")
+    void init() throws IOException {
+        res = new JSONObject(Files.readString(Paths.get("src/test/resources/test_json.json")));
+        System.out.println(res.getJSONObject("res").getJSONArray("paths"));
+    }
+    @Test
+    @DisplayName("JSON res 검증")
+    void validateJson() {
+        assertThat(json.from(res.toString()))
+                .hasJsonPath("$.res");
+    }
+    
+    @Nested
+    @DisplayName("각 파라미터 검증")
+    class validatePaths{
+        JSONArray paths = res.getJSONObject("res").getJSONArray("paths");
+        
+        @Test
+        @DisplayName("paths 검증")
+        void validateEachPath() {
+            System.out.println(paths.toString());
+            assertThat(paths)
+                    .isNotNull();
+            for (Object path : paths) {
+                JSONObject pathObj = (JSONObject) path;
+                assertThat(json.from(pathObj.toString()))
+                        .isNotNull()
+                        .hasJsonPath("$.departureTime");
+                JSONArray legs = ((JSONObject) path).getJSONArray("legs");
+                for (Object leg:legs) {
+                    JSONObject legObj=(JSONObject) leg;
+                    assertThat(json.from(legObj.toString()))
+                            .isNotNull()
+                            .hasJsonPath("$.steps");
+                }
+            }
+        }
+    }
+   
     
     @Test
-    @DisplayName("getPtMap 테스트")
-    void getPtMapTest() {
-        Double departureLongitude = 126.9743832;
-        Double departureLatitude = 37.5666103;
-        Double arrivalLongitude = 127.0093054;
-        Double arrivalLatitude = 37.5550384;
-        String result = naverService.getPtMap(departureLongitude, departureLatitude, arrivalLongitude, arrivalLatitude);
-        assertNotNull(result);
-        assertFalse(result.isEmpty());
-        log.info(result);
+    @DisplayName("DTO로 변환 테스트")
+    void dataTransformTest() {
+//        log.info(res.getJSONObject("res").toString());
+        log.info(res.getJSONObject("res").getJSONArray("paths").getJSONObject(0).get("departureTime").toString());
     }
 }
