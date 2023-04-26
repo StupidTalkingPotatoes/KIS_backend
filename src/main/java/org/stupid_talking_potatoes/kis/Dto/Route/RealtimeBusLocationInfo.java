0f34c4bc64b33ffb33a18dca53f9d246e0bbdc30
hj-language <idgafff010@gmail.com>
2023-04-26 22:29:52 +0900

    [REFACTOR] 디렉터리 구조 변경 및 RealtimeBusLocationInfo, RealtimeBusArrivalInfo 이름 통일 (Response 제거)

--- src/main/java/org/stupid_talking_potatoes/kis/Dto/Route/RealtimeBusLocationInfo.java
+++ src/main/java/org/stupid_talking_potatoes/kis/Dto/Route/RealtimeBusLocationInfo.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  * date : 2023-04-23
  */
 @Data
-public class RealtimeBusLocationResponse {
+public class RealtimeBusLocationInfo {
     private Route route;
     private ArrayList<PassingNode> passingNodeList;
 }
