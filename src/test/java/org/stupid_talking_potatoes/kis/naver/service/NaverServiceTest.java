f89a94e630ee622141739ca5a3958c936093a8d4
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-17 01:52:33 +0900

    [FIX] 테스트 코드 필드 제거

--- src/test/java/org/stupid_talking_potatoes/kis/naver/service/NaverServiceTest.java
+++ src/test/java/org/stupid_talking_potatoes/kis/naver/service/NaverServiceTest.java
@@ -1,5 +1,7 @@
 package org.stupid_talking_potatoes.kis.naver.service;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -9,8 +11,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.json.BasicJsonTester;
-import org.stupid_talking_potatoes.kis.service.NaverService;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -28,15 +30,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 @ExtendWith(MockitoExtension.class)
 class NaverServiceTest {
-    private final NaverService naverService = new NaverService();
     private final BasicJsonTester json = new BasicJsonTester(getClass());
     JSONObject res;
     
     @BeforeEach
     @DisplayName("빠른 길찾기 api 연결")
     void init() throws IOException {
+        LoggerContext logContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        ch.qos.logback.classic.Logger log = logContext.getLogger("com.jayway.jsonpath.internal.path.CompiledPath");
+        log.setLevel(Level.INFO);
         res = new JSONObject(Files.readString(Paths.get("src/test/resources/test_json.json")));
-        System.out.println(res.getJSONObject("res").getJSONArray("paths"));
+//        System.out.println(res.getJSONObject("res").getJSONArray("paths"));
     }
     @Test
     @DisplayName("JSON res 검증")
@@ -48,25 +52,43 @@ class NaverServiceTest {
     @Nested
     @DisplayName("각 파라미터 검증")
     class validatePaths{
-        JSONArray paths = res.getJSONObject("res").getJSONArray("paths");
         
         @Test
         @DisplayName("paths 검증")
         void validateEachPath() {
-            System.out.println(paths.toString());
+            JSONArray paths = res.getJSONObject("res").getJSONArray("paths");
+//            System.out.println(paths.toString());
             assertThat(paths)
                     .isNotNull();
-            for (Object path : paths) {
-                JSONObject pathObj = (JSONObject) path;
-                assertThat(json.from(pathObj.toString()))
+            for (Object pathObj : paths) {
+                JSONObject path = (JSONObject) pathObj;
+                assertThat(json.from(path.toString()))
                         .isNotNull()
                         .hasJsonPath("$.departureTime");
-                JSONArray legs = ((JSONObject) path).getJSONArray("legs");
-                for (Object leg:legs) {
-                    JSONObject legObj=(JSONObject) leg;
-                    assertThat(json.from(legObj.toString()))
+                JSONArray legs = path.getJSONArray("legs");
+                for (Object legObj:legs) {
+                    JSONObject leg=(JSONObject) legObj;
+                    assertThat(json.from(leg.toString()))
                             .isNotNull()
                             .hasJsonPath("$.steps");
+                    JSONArray steps = leg.getJSONArray("steps");
+                    for (Object stepObj : steps) {
+                        JSONObject step=(JSONObject)stepObj;
+                        assertThat(json.from(step.toString()))
+                                .isNotNull()
+                                .hasJsonPath("$.duration")
+                                .hasJsonPath("$.type");
+                        if(step.get("type").toString().equals("BUS")){
+                            JSONObject route = step.getJSONObject("route");
+                            log.info((String) route.get("name"));
+                            JSONArray stations = step.getJSONArray("stations");
+    
+                            JSONObject startNode = (JSONObject) stations.get(0);
+                            JSONObject endNode = (JSONObject) stations.get(1);
+                            log.info((String) startNode.get("name"));
+                            log.info((String) endNode.get("name"));
+                        }
+                    }
                 }
             }
         }
