512ef52aa855a672e4cb9ab4ee27adb198ce467a
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2023-05-22 17:54:17 +0900

    [UPDATE] /search에 사용할 dto 생성

--- src/main/java/org/stupid_talking_potatoes/kis/dto/route/SearchedRoute.java
+++ src/main/java/org/stupid_talking_potatoes/kis/dto/route/SearchedRoute.java
@@ -2,6 +2,7 @@ package org.stupid_talking_potatoes.kis.dto.route;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.stupid_talking_potatoes.kis.entity.Route;
 import org.stupid_talking_potatoes.kis.entity.RouteBase;
 
 /**
@@ -13,6 +14,20 @@ import org.stupid_talking_potatoes.kis.entity.RouteBase;
 @Getter
 @Setter
 public class SearchedRoute extends RouteBase {
-    private String departure;
-    private String arrival;
+    private String startNode;
+    private String endNode;
+
+    /**
+     * RouteEntity -> SearchedRoute (Static Factory Method)
+     * @param route RouteEntity
+     * @return SearchedRoute
+     */
+    public static SearchedRoute fromEntity(Route route) {
+        SearchedRoute searchedRoute = new SearchedRoute();
+        searchedRoute.setRouteId(route.getRouteId());
+        searchedRoute.setRouteNo(route.getRouteNo());
+        searchedRoute.setStartNode(route.getStartNodeName());
+        searchedRoute.setEndNode(route.getEndNodeName());
+        return searchedRoute;
+    }
 }
