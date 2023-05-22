147f81b524aa0e50404c413b73271657a94c78c1
hj-language <idgafff010@gmail.com>
2023-05-22 16:56:03 +0900

    [update] add factory method

--- src/main/java/org/stupid_talking_potatoes/kis/entity/Node.java
+++ src/main/java/org/stupid_talking_potatoes/kis/entity/Node.java
@@ -2,11 +2,9 @@ package org.stupid_talking_potatoes.kis.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.stupid_talking_potatoes.kis.dto.tago.TAGO_AroundNodeInfo;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -22,7 +20,7 @@ import java.util.Set;
 @Entity
 @ToString(callSuper = true)
 @SuperBuilder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Node extends NodeBase {
     private Double longitude;
     private Double latitude;
@@ -35,5 +33,16 @@ public class Node extends NodeBase {
         super(nodeId, nodeName, nodeNo);
         this.longitude = longitude;
         this.latitude = latitude;
+    }
+
+    public static Node of(TAGO_AroundNodeInfo aroundNodeInfo) {
+        Node node = new Node(
+                aroundNodeInfo.getNodeId(),
+                aroundNodeInfo.getNodeNm(),
+                aroundNodeInfo.getNodeNo().toString(),
+                aroundNodeInfo.getGpsLong(),
+                aroundNodeInfo.getGpsLati()
+        );
+        return node;
     }
 }
