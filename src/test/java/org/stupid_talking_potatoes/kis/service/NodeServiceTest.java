d5333ec6415a703a939d892bca73579abcac49c4
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-24 19:42:41 +0900

    Merge

--- src/test/java/org/stupid_talking_potatoes/kis/service/NodeServiceTest.java
+++ src/test/java/org/stupid_talking_potatoes/kis/service/NodeServiceTest.java
@@ -15,9 +15,10 @@ import org.stupid_talking_potatoes.kis.entity.Node;
 import org.stupid_talking_potatoes.kis.repository.NodeRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -25,13 +26,13 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class NodeServiceTest {
     private static NodeService nodeService;
-
+    
     private static NodeRepository nodeRepository;
-
+    
     private static RouteService routeService;
-
+    
     private static TAGOService tagoService;
-
+    
     @BeforeAll
     public static void set() {
         nodeRepository = Mockito.mock(NodeRepository.class);
@@ -39,7 +40,7 @@ public class NodeServiceTest {
         routeService = Mockito.mock(RouteService.class);
         nodeService = new NodeService(nodeRepository, tagoService, routeService);
     }
-
+    
     @Nested
     @DisplayName("getNodeList 메서드")
     class getNodeList {
@@ -50,37 +51,37 @@ public class NodeServiceTest {
             String name = "옥계중학교";
             Node node1 = new Node("1", "옥계중학교건너", "1", 1.1, 1.1);
             Node node2 = new Node("2", "옥계중학교앞", "2", 1.1, 1.1);
-
+            
             ArrayList<Node> nodes = new ArrayList<Node>();
             nodes.add(node1); nodes.add(node2);;
-
+            
             when(nodeRepository.findByNodeNameContaining(name)).thenReturn(nodes);
-
+            
             // when
             ArrayList<NodeDto> response = nodeService.getNodeList(null, name);
-
+            
             // then
             assertEquals(2, response.size());
         }
-
+        
         @Test
         @DisplayName("getNodeList 테스트 - no로 검색 - 성공")
         void getNodeByNo() {
             // given
             String no = "10131";
             Node node = new Node("1", "옥계중학교건너", "10131", 1.1, 1.1);
-
+            
             when (nodeRepository.findByNodeNo(no)).thenReturn(Optional.of(node));
-
+            
             // when
             ArrayList<NodeDto> response = nodeService.getNodeList(no, null);
-
+            
             // then
             assertEquals(1, response.size());
             assertEquals("10131", response.get(0).getNo());
         }
     }
-
+    
     @Nested
     @DisplayName("getRealtimeBusArrivalInfo 메서드")
     class getRealtimeBusArrivalInfo {
@@ -92,26 +93,69 @@ public class NodeServiceTest {
             Node node = new Node("GMB131", "옥계중학교건너", "10131", 1.1, 1.1);
             ArrivalRoute arrivalRoute1 = new ArrivalRoute("1", "190", 3, 180);
             ArrivalRoute arrivalRoute2 = new ArrivalRoute("2", "195", 10, 600);
-
+            
             ArrayList<ArrivalRoute> routes = new ArrayList<ArrivalRoute>();
             routes.add(arrivalRoute1); routes.add(arrivalRoute2);
-
+            
             when (nodeRepository.findById(id)).thenReturn(Optional.of(node));
             when (tagoService.requestRealtimeBusArrivalInfo(id)).thenReturn(routes);
-            when (routeService.getDeparture(any(String.class))).thenReturn("구미역");
-
+            when (routeService.getArrivalName(any(String.class))).thenReturn("구미역");
+            
             // when
             RealtimeBusArrivalInfo response = nodeService.getRealtimeBusArrivalInfo(id);
-
+            
             // then
             assertEquals(2, response.getArrivalRouteList().size());
-
+            assertEquals(180, response.getArrivalRouteList().get(0).getArrTime()); // sorting test
         }
-
+        
         @Test
         @DisplayName("getRealtimeBusArrivalInfo 테스트 - 실패 (Invalid ID)")
         void getInfoWithInvalidId() {
-
+        
+        }
+    }
+    
+    @Nested
+    @DisplayName("getAroundNodeInfo 메서드")
+    class getAroundNodeInfo {
+        @Test
+        @DisplayName("getAroundNodeInfo 테스트 - 성공 (주변 정류장이 있는 경우)")
+        void getNodes() {
+            // given
+            Double latitude = 36.140382;
+            Double longitude = 128.3964849;
+            
+            ArrayList<Node> nodes = new ArrayList<>();
+            nodes.add(new Node("GMB131", "금오공대입구(옥계중학교방면)", "10131", 128.3967393, 36.13948442));
+            nodes.add(new Node("GMB130", "금오공대입구(금오공대종점방면)", "10130", 128.3971135, 36.13929449));
+            nodes.add(new Node("GMB132", "금오공대종점", "10132", 128.394333, 36.142569));
+            
+            when (tagoService.requestAroundNodeInfo(longitude, latitude)).thenReturn(nodes);
+            
+            // when
+            List<NodeDto> response = nodeService.getAroundNodeInfo(longitude, latitude);
+            
+            // then
+            assertEquals(3, response.size());
+        }
+        
+        @Test
+        @DisplayName("getAroundNodeInfo 테스트 - 성공 (주변 정류장이 없는 경우)")
+        void noAroundNode() {
+            // given
+            Double latitude = 36.1702;
+            Double longitude = 128.3903;
+            
+            ArrayList<Node> nodes = new ArrayList<>();
+            
+            when (tagoService.requestAroundNodeInfo(longitude, latitude)).thenReturn(nodes);
+            
+            // when
+            List<NodeDto> response = nodeService.getAroundNodeInfo(longitude, latitude);
+            
+            // then
+            assertEquals(0, response.size());
         }
     }
 }
