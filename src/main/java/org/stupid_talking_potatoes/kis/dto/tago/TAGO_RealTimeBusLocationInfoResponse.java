191d014af8e8b1f0c249ec8fcd975f5dcd4f24ca
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-24 19:52:15 +0900

    Merge

--- src/main/java/org/stupid_talking_potatoes/kis/dto/tago/TAGO_RealTimeBusLocationInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package org.stupid_talking_potatoes.kis.dto.tago;
-
-import lombok.Getter;
-
-@Getter
-public class TAGO_RealTimeBusLocationInfoResponse {
-    private TAGO_ApiResponse<TAGO_RealTimeBusLocationInfo> response;
-
-}
