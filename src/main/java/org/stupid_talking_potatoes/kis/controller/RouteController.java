a5e8e3d4c95c3cdd919c11cf2275325ac3b69fdc
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2023-05-31 23:04:41 +0900

    [FIX] api 및 메소드명 수정

--- src/main/java/org/stupid_talking_potatoes/kis/controller/RouteController.java
+++ src/main/java/org/stupid_talking_potatoes/kis/controller/RouteController.java
@@ -1,19 +1,13 @@
 package org.stupid_talking_potatoes.kis.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.stupid_talking_potatoes.kis.dto.route.RealtimeBusLocationInfo;
 import org.stupid_talking_potatoes.kis.dto.route.SearchedRoute;
-import org.stupid_talking_potatoes.kis.entity.Route;
 import org.stupid_talking_potatoes.kis.service.RouteService;
 
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,16 +17,16 @@ import java.util.List;
  * date : 2023-04-23
  */
 @RestController
-@RequestMapping("/routes")
+@RequestMapping("/api/routes")
 @RequiredArgsConstructor
 public class RouteController {
     private final RouteService routeService;
     @GetMapping("/search")
-    public List<SearchedRoute> getRouteList(String keyword){
-        return routeService.getRouteList(keyword);
+    public List<SearchedRoute> getRouteList(String routeNo){
+        return routeService.getRouteList(routeNo);
     }
     @GetMapping("/location")
     public RealtimeBusLocationInfo getRealtimeBusLocationInfo(String routeId) {
-        return routeService.getBusRouteInfo(routeId);
+        return routeService.getRealtimeBusLocationInfo(routeId);
     }
 }
