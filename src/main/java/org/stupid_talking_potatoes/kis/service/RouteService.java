63f88e21d6344428d2655341866a3306c3d60fcc
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2023-05-31 23:06:50 +0900

    [UPDATE & REFACTOR] api 구현 부분 db로 구현하도록 수정 및 리팩토링

--- src/main/java/org/stupid_talking_potatoes/kis/service/RouteService.java
+++ src/main/java/org/stupid_talking_potatoes/kis/service/RouteService.java
@@ -8,9 +8,12 @@ import org.stupid_talking_potatoes.kis.dto.route.RealtimeBusLocationInfo;
 import org.stupid_talking_potatoes.kis.dto.route.SearchedRoute;
 import org.stupid_talking_potatoes.kis.dto.tago.TAGO_RealTimeBusLocationInfo;
 import org.stupid_talking_potatoes.kis.entity.Route;
+import org.stupid_talking_potatoes.kis.entity.RouteSeq;
 import org.stupid_talking_potatoes.kis.repository.RouteRepository;
 
 import java.util.*;
+
+import static java.util.Comparator.comparingInt;
 
 /**
  * package :  org.stupid_talking_potatoes.kis.route.service
@@ -21,7 +24,6 @@ import java.util.*;
 @Service
 @RequiredArgsConstructor
 public class RouteService {
-
     private final RouteRepository routeRepository;
 
     private final KneelingBusService kneelingBusService;
@@ -29,24 +31,20 @@ public class RouteService {
     private final TAGOService tagoService;
 
     /**
-     * searchApi: keyword를 가지고 Route 검색
-     * @param keyword 검색하고자 하는 keyword(RouteNo)
+     * searchApi: routeNo를 가지고 Route 검색
+     * @param routeNo 검색하고자 하는 routeNo(RouteNo)
      * @return SearchedRouteList
      */
-    public List<SearchedRoute> getRouteList(String keyword){
-        if (StringUtils.isNullOrEmpty(keyword)) {
+    public List<SearchedRoute> getRouteList(String routeNo){
+        if (StringUtils.isNullOrEmpty(routeNo)) {
             return Collections.emptyList();
         }
 
-        List<Route> routes = routeRepository.findAllByRouteNoContaining(keyword);
+        List<Route> routes = routeRepository.findAllByRouteNoContaining(routeNo);
 
-        List<SearchedRoute> searchedRoutes = new ArrayList<>();
-        for (Route route : routes) {
-            SearchedRoute searchedRoute = SearchedRoute.fromEntity(route);
-            searchedRoutes.add(searchedRoute);
-        }
-
-        return searchedRoutes;
+        return routes.stream()
+                .map(SearchedRoute::fromEntity)
+                .toList();
     }
 
     /**
@@ -54,14 +52,18 @@ public class RouteService {
      * @param routeId routeId
      * @return RealtimeBusLocationInfo
      */
-    public RealtimeBusLocationInfo getBusRouteInfo(String routeId) {
-
-        List<PassingNode> passingNodeList = tagoService.getPassingNodeList(routeId);
-
+    public RealtimeBusLocationInfo getRealtimeBusLocationInfo(String routeId) {
         Optional<Route> routeOp = routeRepository.findByRouteId(routeId);
-    
+
         Route route = routeOp.orElseThrow(() -> new NoSuchElementException(routeId + "에 해당하는 노선을 찾을 수 없습니다"));
-    
+
+        Set<RouteSeq> routeNodes = route.getRouteNodes();
+
+        List<PassingNode> passingNodeList = routeNodes.stream()
+                .map(PassingNode::fromRouteSeq)
+                .sorted(comparingInt(PassingNode::getOrder))
+                .toList();
+
         List<TAGO_RealTimeBusLocationInfo> realTimeBusLocationInfoList = tagoService.requestRealTimeBusLocationInfo(routeId);
     
         // 저상버스 필터링
@@ -69,8 +71,9 @@ public class RouteService {
     
         return RealtimeBusLocationInfo.of(route, passingNodeList, realtimeNodeOrderList);
     }
+
     public String getArrivalName(String routeId){
-        Route route = routeRepository.findByRouteId(routeId).orElseThrow(()-> new NoSuchElementException());
+        Route route = routeRepository.findByRouteId(routeId).orElseThrow(NoSuchElementException::new);
         return route.getEndNodeName();
     }
 
@@ -80,15 +83,10 @@ public class RouteService {
      * @return realtimeNodeOrderList(현재 운행 중인 저상버스 위치 리스트)
      */
     private List<Integer> getFilteredNodeOrderList(List<TAGO_RealTimeBusLocationInfo> realTimeBusLocationInfoList) {
-        List<Integer> realtimeNodeOrderList = new ArrayList<>();
-
-        for (TAGO_RealTimeBusLocationInfo realTimeBusLocationInfo : realTimeBusLocationInfoList) {
-            String vehicleNo = realTimeBusLocationInfo.getVehicleNo();
-            if (kneelingBusService.isKneelingBus(vehicleNo)) {
-                realtimeNodeOrderList.add(realTimeBusLocationInfo.getNodeOrd());
-            }
-        }
-        return realtimeNodeOrderList;
+        return realTimeBusLocationInfoList.stream()
+                .filter(realTimeBusLocationInfo -> kneelingBusService.isKneelingBus(realTimeBusLocationInfo.getVehicleNo()))
+                .map(TAGO_RealTimeBusLocationInfo::getNodeOrd)
+                .toList();
     }
 
     public String getDeparture(String routeId){
