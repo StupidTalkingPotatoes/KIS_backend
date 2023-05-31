f581b60674732443e1d8f0b65a8250c62e1f103f
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2023-05-31 23:05:04 +0900

    [FIX] 변수명 수정

--- src/main/java/org/stupid_talking_potatoes/kis/repository/RouteRepository.java
+++ src/main/java/org/stupid_talking_potatoes/kis/repository/RouteRepository.java
@@ -1,8 +1,10 @@
 package org.stupid_talking_potatoes.kis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.stupid_talking_potatoes.kis.entity.Route;
+import org.stupid_talking_potatoes.kis.entity.RouteSeq;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +13,11 @@ public interface RouteRepository extends JpaRepository<Route, String> {
     List<Route> findByRouteNo(@NonNull String routeNo);
 
     /**
-     * 검색하고자 하는 keyword를 사용하여 RouteNo 기준으로 Like 검색
-     * @param keyword routeNo 검색어
-     * @return RouteNo에 keyword가 포함된 RouteList
+     * 검색하고자 하는 routeNo를 사용하여 RouteNo 기준으로 Like 검색
+     * @param routeNo routeNo 검색어
+     * @return routeNo가 포함된 RouteList
      */
-    List<Route> findAllByRouteNoContaining(String keyword);
+    List<Route> findAllByRouteNoContaining(String routeNo);
 
     Optional<Route> findByRouteId(String routeId);
 }
