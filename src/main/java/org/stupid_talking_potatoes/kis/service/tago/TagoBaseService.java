c0e3e7cca598b56a0ce0a840b9783c98d91bb4a8
hj-language <idgafff010@gmail.com>
2023-06-04 22:56:56 +0900

    [FIX] convert 함수 수정

--- src/main/java/org/stupid_talking_potatoes/kis/service/tago/TagoBaseService.java
+++ src/main/java/org/stupid_talking_potatoes/kis/service/tago/TagoBaseService.java
@@ -2,11 +2,9 @@ package org.stupid_talking_potatoes.kis.service.tago;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
 import org.json.XML;
@@ -14,11 +12,14 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.stupid_talking_potatoes.kis.dto.tago.TAGO_RealTimeBusLocationInfo;
 import org.stupid_talking_potatoes.kis.exception.InternalServerException;
 import org.stupid_talking_potatoes.kis.exception.ThirdPartyAPIException;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * TAGO API에 접근하는 Base 서비스
@@ -76,10 +77,11 @@ public abstract class TagoBaseService<T, U> {
     /**
      * Json body를 T가 담긴 리스트로 변환
      * @param body Json body
-     * @param typeReference 바꿀 타입 레퍼런스
+     * @param singleReference 바꿀 타입 레퍼런스 (T)
+     * @param listReference 바꿀 타입 레퍼런스 (List<T>)
      * @return body에서 추출한 T 리스트
      */
-    protected ArrayList<T> convert(String body, TypeReference<ArrayList<T>> typeReference) {
+    protected List<T> convert(String body, TypeReference<T> singleReference, TypeReference<List<T>> listReference) {
         ObjectMapper objectMapper = new ObjectMapper()
                 // fields of dto are camelCase, but fields of TAGO api are lowercase
                 .setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CASE);
@@ -99,14 +101,22 @@ public abstract class TagoBaseService<T, U> {
             // get body -> check items count
             JsonNode responseBody = jsonNode.get("response").get("body");
             if (responseBody.get("totalCount").asInt() == 0) { // when items is empty
-                return new ArrayList<>();
+                return Collections.emptyList();
             }
 
-            // convert items to object list & return
-            ArrayNode arrayNode = (ArrayNode) responseBody.get("items").get("item");
+            // convert items to object list
+            JsonNode items = responseBody.get("items");
+            JsonNode item = items.get("item");
+            JsonNodeType nodeType = item.getNodeType();
 
-            ArrayList<T> aroundNodeList = objectMapper.convertValue(arrayNode, typeReference);
-            return aroundNodeList;
+            if (nodeType.equals(JsonNodeType.OBJECT)) {
+                T info = objectMapper.convertValue(item, singleReference);
+                return Collections.singletonList(info);
+            } else if (nodeType.equals(JsonNodeType.ARRAY)) {
+                return objectMapper.convertValue(item, listReference);
+            }
+
+            return Collections.emptyList();
 
         } catch (JsonMappingException e) {
             throw InternalServerException.builder()
@@ -132,5 +142,5 @@ public abstract class TagoBaseService<T, U> {
      * @param list 조건에 맞게 필터링할 리스트
      * @return 필터링 결과
      */
-    protected abstract ArrayList<U> filter(ArrayList<T> list);
+    protected abstract List<U> filter(List<T> list);
 }
